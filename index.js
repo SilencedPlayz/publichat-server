97565a6c2d46467ddd4d779f62356c05a310c130
Silenced CODE <sillencedplayz@gmail.com>
2025-11-11 03:45:10 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import http from "http";
+import { WebSocketServer } from "ws";
+
+const server = http.createServer();
+const wss = new WebSocketServer({ noServer: true });
+
+server.on("upgrade", (req, socket, head) => {
+  wss.handleUpgrade(req, socket, head, (ws) => {
+    wss.emit("connection", ws, req);
+  });
+});
+
+wss.on("connection", (ws) => {
+  console.log("Connection has been made");
+  const config = {type: "log", username: "Server", msg: "Successfully Connected"}
+  ws.send(JSON.stringify(config));
+
+  ws.on("message", (msg) => {
+    const message = msg.toString();
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Connection lost");
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server running...");
+});
